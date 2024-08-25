9ea5bccb9f81b51bb60d82ade59a21e894d04d0b
Official-Krish <krishanand0103@gmail.com>
2024-08-25 18:03:35 +0530

    added orderRouter

---
+++ Exchange/apps/api/src/index.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+import cors from 'cors';
+import { orderRouter } from './routes/order';
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.use("/api/v1/order", orderRouter);
+
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
